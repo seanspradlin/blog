257373202f8ebeead6b04060476ca1a859c86dc1
Sean Spradlin <sean@seanspradlin.com>
2026-06-05 20:52:37 -0500

    feat: configure for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,4 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-let veliteStarted = false;
-function velite() {
-	return {
-		name: 'velite',
-		async configResolved() {
-			if (veliteStarted) return;
-			veliteStarted = true;
-			const { build } = await import('velite');
-			await build({ watch: process.env.NODE_ENV === 'development', clean: true });
-		}
-	};
-}
-
-export default defineConfig({ plugins: [tailwindcss(), velite(), sveltekit()] });
+export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
