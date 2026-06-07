2567eda43e96879b4e3035e6b22b787ea2987d01
Sean Spradlin <sean@seanspradlin.com>
2026-06-07 12:50:45 -0500

    fix: fixed overflow

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -2,27 +2,27 @@
 	import profile from '$lib/assets/profile.webp';
 </script>
 
-<section id="hero" class="mx-auto max-w-5xl lg:my-48">
+<section id="hero" class="mx-auto max-w-5xl md:my-8 lg:my-16 xl:my-32">
 	<h1 class="mb-8 font-display text-4xl font-black text-rp-iris lg:text-6xl">Welcome</h1>
-	<div class="float-right mb-2 ml-4 aspect-square w-48 md:w-56 lg:w-64" aria-hidden="true">
-		<img
-			src={profile}
-			alt="Portrait of Sean Spradlin"
-			fetchpriority="high"
-			class="border-subtle block aspect-square w-full rounded-xl border-4 object-cover object-top shadow-md saturate-75"
-		/>
-	</div>
-	<div class="text-muted flex max-w-prose flex-col gap-4 font-body">
-		<p>
+	<div class="text-muted font-body">
+		<div class="float-right mb-2 ml-4 aspect-square w-48 md:w-56 lg:w-64" aria-hidden="true">
+			<img
+				src={profile}
+				alt="Portrait of Sean Spradlin"
+				fetchpriority="high"
+				class="border-subtle block aspect-square w-full rounded-xl border-4 object-cover object-top shadow-md saturate-75"
+			/>
+		</div>
+		<p class="mb-4">
 			I'm <strong class="text-text-strong text-rp-rose">Sean Spradlin</strong>, a substitute
 			teacher-turned-programmer. I've been doing this professionally since jQuery and Bootstrap were
 			hot, and unprofessionally since SimCity 2000.
 		</p>
-		<p>
+		<p class="mb-4">
 			In this space, you will find some of my hard-coded side projects, my vibe-coded side projects,
 			general brain dumps, and shameless self-promotion.
 		</p>
-		<p>My long-term goal is to bring back GeoCities.</p>
+		<p class="mb-4">My long-term goal is to bring back GeoCities.</p>
 	</div>
 	<ul class="my-8 flex items-center gap-4">
 		<li>
